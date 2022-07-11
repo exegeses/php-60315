512cfafe26d2f6d06a5e8eb5e82851af8018462f
Marcos <exegeses@gmail.com>
2022-07-11 20:19:31 -0300

    vista de reporta de alta ok/error

---
+++ catalogo/agregarMarca.php
@@ -0,0 +1,17 @@
+<?php
+    //require 'config/config.php';
+    include 'layout/header.php';
+    include 'layout/nav.php';
+?>
+
+    <main class="container py-4">
+        <h1>Alta de una marca</h1>
+        <div class="alert bg-light p-4 col-8 mx-auto shadow">
+            Marca agregada correctamente.
+            
+        </div>
+    </main>
+
+<?php
+    include 'layout/footer.php';
+?>
