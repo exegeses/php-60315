127d04b344096cdad6edd6b3ae44e4d1f31783b1
Marcos <exegeses@gmail.com>
2022-07-14 22:03:59 -0300

    Panel de administración de productos

--- catalogo/adminProductos.php
+++ catalogo/adminProductos.php
@@ -1,6 +1,9 @@
 <?php
 
     //require 'config/config.php';
+    require 'funciones/conexion.php';
+    require 'funciones/productos.php';
+    $productos = listarProductos();
 	include 'layout/header.php';
 	include 'layout/nav.php';
 ?>
@@ -19,10 +22,10 @@
                     <th>Precio</th>
                     <th>Marca</th>
                     <th>Categoria</th>
-                    <th>Presentación</th>
+                    <th>Descripción</th>
                     <th>Imagen</th>
                     <th colspan="2">
-                        <a href="" class="btn btn-outline-secondary">
+                        <a href="formAgregarProducto.php" class="btn btn-outline-secondary">
                             Agregar
                         </a>
                     </th>
@@ -30,28 +33,28 @@
             </thead>
             <tbody>
 <?php
-
+        while ( $producto = mysqli_fetch_assoc($productos) ){
 ?>            
                 <tr>
-                    <td>producto</td>
-                    <td>Precio</td>
-                    <td>Marca</td>
-                    <td>Categoria</td>
-                    <td>Presentación</td>
-                    <td>Imagen</td>
+                    <td><?= $producto['prdNombre'] ?></td>
+                    <td><span class="precio3">$<?= $producto['prdPrecio'] ?></span></td>
+                    <td><?= $producto['mkNombre'] ?></td>
+                    <td><?= $producto['catNombre'] ?></td>
+                    <td><?= $producto['prdDescripcion'] ?></td>
+                    <td><img src="productos/<?= $producto['prdImagen'] ?>" class="img-thumbnail"></td>
                     <td>
-                        <a href="" class="btn btn-outline-secondary">
+                        <a href="formModificarProducto.php?idProducto=<?= $producto['idProducto'] ?>" class="btn btn-outline-secondary">
                             Modificar
                         </a>
                     </td>
                     <td>
-                        <a href="" class="btn btn-outline-secondary">
+                        <a href="formEliminarProducto.php?idProducto=<?= $producto['idProducto'] ?>" class="btn btn-outline-secondary">
                             Eliminar
                         </a>
                     </td>
                 </tr>
 <?php
-
+        }
 ?>
             </tbody>
         </table>
--- catalogo/funciones/productos.php
+++ catalogo/funciones/productos.php
@@ -2,6 +2,38 @@
 
 ### CRUD de productos  ###
 
+function listarProductos() : mysqli_result | false
+{
+    $link = conectar();
+    /*
+    $sql = "SELECT
+                    idProducto, prdNombre, prdPrecio,
+                    marcas.idMarca, mkNombre,
+                    categorias.idCategoria, catNombre,
+                    prdDescripcion, prdImagen
+            FROM productos, marcas, categorias
+            WHERE productos.idMarca = marcas.idMarca
+              AND productos.idCategoria = categorias.idCategoria;";*/
+    $sql = "SELECT
+                    idProducto, prdNombre, prdPrecio,
+                    m.idMarca, mkNombre,
+                    c.idCategoria, catNombre,
+                    prdDescripcion, prdImagen
+                FROM productos p
+                    JOIN marcas m
+                        ON p.idMarca = m.idMarca
+                  JOIN categorias c
+                        ON  p.idCategoria = c.idCategoria";
+    try
+    {
+        $resultado = mysqli_query($link, $sql);
+        return $resultado;
+    }catch ( Exception $e )
+    {
+        echo $e->getMessage();
+        return false;
+    }
+}
 
 /*
  * listarProductos()
