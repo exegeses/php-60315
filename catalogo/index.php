d6f02f90df1dcf6b8835170dcb12ded9c9f2642f
Marcos <exegeses@gmail.com>
2022-07-28 20:18:42 -0300

    vista de la partida del proyecto

--- catalogo/index.php
+++ catalogo/index.php
@@ -1,5 +1,8 @@
 <?php
 //require 'config/config.php';
+    require 'funciones/conexion.php';
+    require 'funciones/productos.php';
+    $productos = listarProductos();
     include 'layout/header.php';
     include 'layout/nav.php';
 ?>
@@ -8,19 +11,24 @@
         <h1>Catálogo de productos</h1>
         <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4 py-5">
 
-
+<?php
+        while( $producto = mysqli_fetch_assoc( $productos ) )
+        {
+?>
             <article class="col d-flex align-items-start">
                 <div class="m-1 producto">
-                    <img src="productos/noDisponible.png">
-                    <h2><?= 'prdNombre' ?></h2>
-                    <span class="precio">$<?= 'prdPrecio' ?></span>
+                    <img src="productos/<?= $producto['prdImagen'] ?>">
+                    <h2><?= $producto['prdNombre'] ?></h2>
+                    <span class="precio">$<?= $producto['prdPrecio'] ?></span>
                     <p>
-                        Marca: <?= 'mkNombre' ?> <br>
-                        Categoría: <?= 'catNombre' ?> <br>
+                        Marca: <?= $producto['mkNombre'] ?> <br>
+                        Categoría: <?= $producto['catNombre'] ?> <br>
                     </p>
                 </div>
             </article>
-
+<?php
+        }
+?>
         </div>
         
     </main>
