cd06b9d33f8a4105e8c3808fa235db411ea7abbc
Marcos <exegeses@gmail.com>
2022-08-08 20:33:41 -0300

    vista noAdmin

---
+++ catalogo/noAdmin.php
@@ -0,0 +1,18 @@
+<?php
+    require 'config/config.php';
+    include 'layout/header.php';
+    include 'layout/nav.php';
+?>
+
+    <main class="container py-4">
+        <h1>¡Advertencia!</h1>
+
+        <p class="alert shadow col-8 mx-auto ">
+            Debe ser administrador para realizar esta tarea.
+        </p>
+
+    </main>
+
+<?php
+    include 'layout/footer.php';
+?>
